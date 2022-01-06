830f4fe7667d8b50066b187b867d7195db94a922
Santiago Suárez <santiago.suarez@axcelsoftware.com>
2022-01-06 12:08:10 -0500

    Feature: Verify iat from data token

--- app/Middleware/VerifyToken.ts
+++ app/Middleware/VerifyToken.ts
@@ -1,23 +1,23 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { decodeJWT, getToken } from 'App/Utils/functions/jwt';
+import { getToken } from 'App/Utils/functions/jwt';
 import axios from 'axios';
 import Env from '@ioc:Adonis/Core/Env';
-import { IDataToken, IResponseData } from 'App/Utils/interfaces';
+import { IResponseData } from 'App/Utils/interfaces';
 
 export default class VerifyToken {
     public async handle({ request, response }: HttpContextContract, next: () => Promise<void>) {
         let responseData: IResponseData = {
             message: 'Debe de ingresar para realizar esta acción.',
             error: true,
-            status: 200,
+            status: 401,
         };
-        const { token, headerAuthorization } = getToken(request.headers());
+        const { token, headerAuthorization, payloadToken } = getToken(request.headers());
 
-        // Get data of Token
-        let payload: IDataToken = { id: -1, iat: -1 };
-        if (token !== '') payload = decodeJWT(token);
-
-        if (token === '' || (payload['iat'] === -1 && payload['id'] === -1)) {
+        if (
+            token === '' ||
+            !payloadToken['iat'] ||
+            (payloadToken['iat'] === -1 && payloadToken['id'] === -1)
+        ) {
             return response.unauthorized(responseData);
         }
 
